a25ea9e7cd928e3a40b02807e924c1751b32295a
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-24 10:21:45 +0530

    Initial Commit

---
+++ CustomTransition/CustomTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTransitionApp.swift
+//  CustomTransition
+//
+//  Created by shehan karunarathna on 2022-02-24.
+//
+
+import SwiftUI
+
+@main
+struct CustomTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
